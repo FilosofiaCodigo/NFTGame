aa43234fb1c5f531f7fe07346162402d5b348d11
turupawn <ahmed.hn.43@gmail.com>
2021-10-31 13:20:05 -0600

    truffle stuff

---
+++ contracts/MyNFTs.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract MyNFTs is ERC721 {
+    uint256 token_count;
+
+    constructor() ERC721("My NFT", "MNFT") {}
+
+    function mintNFT(address to) public
+    {
+        token_count  += 1;
+        _mint(to, token_count);
+    }
+}
